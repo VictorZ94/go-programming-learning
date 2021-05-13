a834e3486526905237852c9c47a3d59a15c3b0a4
Victor Zuluaga <zrvictor@outlook.com>
2021-05-13 13:04:52 -0500

    Using Println to print standard output

---
+++ 0x00-hello_world/println.go
@@ -0,0 +1,12 @@
+package main
+
+/**
+ * main - Enter point
+ * print first string standard output
+ * Return: Always 0
+ */
+import "fmt"
+
+func main() {
+	fmt.Println("with proper grammar, but the outcome is a piece of art,")
+}
