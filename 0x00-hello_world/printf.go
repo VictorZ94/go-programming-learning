afb76e1e3fb0f285e922e912d040e3af71db9ae0
Victor Zuluaga <zrvictor@outlook.com>
2021-05-13 13:04:15 -0500

    Using printf to print standard output

---
+++ 0x00-hello_world/printf.go
@@ -0,0 +1,12 @@
+package main
+
+/**
+ * main - Enter point
+ * print first string standard output
+ * Return: Always 0
+ */
+import "fmt"
+
+func main() {
+	fmt.Printf("with proper grammar, but the outcome is a piece of art,\n")
+}
